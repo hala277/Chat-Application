8811cdfefbcea9f57c49cf97c6c5574551163dc7
hala277 <hala.almasharfeh@gmail.com>
2021-10-25 00:24:15 +0300

    start the app

---
+++ my-app/src/app.js
@@ -0,0 +1,16 @@
+import {ChatEngine, chatEngine} from 'react-chat-engine';
+import './app.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+        height = "100vh"
+        projectID=""
+        userName=""
+        userSecret=""
+
+        />
+    );
+}
+
+export default App;
