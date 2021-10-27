953baa6b055d17ac92a0ca9cf9e089a51adfd18e
hala277 <hala.almasharfeh@gmail.com>
2021-10-28 00:04:30 +0300

    add

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,14 +1,14 @@
-import {ChatEngine, chatEngine} from 'react-chat-engine';
+import {ChatEngine} from 'react-chat-engine';
 import './app.css';
-
+import ChatFeed from './components/ChatFeed';
 const App = () => {
     return(
         <ChatEngine
         height = "100vh"
-        projectID=""
-        userName=""
-        userSecret=""
-
+        projectID="15605cc0-da43-4b1b-9000-fddd79cf9a0b"
+        userName="hala"
+        userSecret="123123"
+        renderChatFeed={(chatAppProps) => < ChatFeed { ... chatAppProps } /> }
         />
     );
 }
